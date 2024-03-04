f300a0f42f9cd1105b40bf6644100ec08acdaf4c
Albina <kamalieva.albish@mail.ru>
2024-03-04 13:06:07 +0300

    Анкета

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -9,16 +9,22 @@ namespace ConsoleApp3
 {
     internal class Program
     {
-        static void Main(string[] args)
+       public static void Main(string[] args)
         {
-           const string MyName = "Albina";
-            Console.WriteLine(MyName);
-            Console.WriteLine("Привет, \n мир");
-            Console.WriteLine("\t Мне 36 лет");
-            Console.WriteLine("\t My name is \n Albina");
-            Console.WriteLine("\u0023");
+            string MyName = "Albina";
+            byte MyAge = 36;
+            bool HaveIApet = true;
+            double MyShoeSize = 36.5;
+
+            Console.WriteLine("My name is" + MyName);
+            Console.WriteLine("My age is" + MyAge);
+            Console.WriteLine("Do I have a pet?" + HaveIApet);
+            Console.WriteLine("My shoe size is" + MyShoeSize);
+
             Console.ReadLine();
         }
     }
 }
-git commit -a
+
+            
+ 
